e28f0e54eae08bcea137b3357a138bcf35240f8b
ppudding3861 <ppuding3861@gmail.com>
2024-08-19 19:45:10 +0900

    feat : 유저엔티티에 빌드패턴 추가

--- src/main/java/com/ohgiraffers/pizzahouse/user/model/UserEntity.java
+++ src/main/java/com/ohgiraffers/pizzahouse/user/model/UserEntity.java
@@ -3,6 +3,8 @@ package com.ohgiraffers.pizzahouse.user.model;
 
 import jakarta.persistence.*;
 
+import java.util.regex.Pattern;
+
 @Entity
 @Table(name = "tbl_user")
 public class UserEntity {
@@ -15,7 +17,7 @@ public class UserEntity {
     private String userName; // 이름
 
     @Column(name = "user_age", nullable = false)
-    private String userAge; // 나이
+    private Integer userAge; // 나이
 
     @Column(name = "user_postcode")
     private int postCode; // 우편번호
@@ -29,52 +31,81 @@ public class UserEntity {
     public UserEntity() {
     }
 
-    public Integer getUserId() {
-        return userId;
+    public static class Builder {
+        private String userName;
+        private Integer userAge;
+        private int postCode;
+        private String adderess;
+        private String adderessDetail;
+
+        public UserEntity builder() {
+            if (userName == null || userAge == null || adderess == null || adderessDetail == null) {
+                throw new IllegalArgumentException("곳간을 채워주세요");
+            }
+            return new UserEntity(this);
+        }
+
+        public Builder setUserName(String userName) {
+            Pattern pattern = Pattern.compile("^[가-힣]+$");
+            if (userName == null || userName.isEmpty()) {
+                throw new IllegalArgumentException("입력해 주세요");
+            } else if (!pattern.matcher(userName).matches()) {
+                throw new IllegalArgumentException("한글로 입력해 주세요");
+            } else if (userName.length() > 3) {
+                throw new IllegalArgumentException("3글자 이하로 입력해 주세요");
+            }else {
+                this.userName = userName;
+                return this;
+            }
+        }
+        public Builder setUserAge(Integer userAge) {
+            this.userAge = userAge;
+            return this;
+        }
+        public Builder setPostCode(int postCode) {
+            this.postCode = postCode;
+            return this;
+        }
+        public Builder setAdderess(String adderess) {
+            this.adderess = adderess;
+            return this;
+        }
+        public Builder setAdderessDetail(String adderessDetail) {
+            this.adderessDetail = adderessDetail;
+            return this;
+        }
+
+    }
+    public UserEntity(Builder builder) {
+        this.userName = builder.userName;
+        this.userAge = builder.userAge;
+        this.postCode = builder.postCode;
+        this.adderess = builder.adderess;
+        this.adderessDetail = builder.adderessDetail;
     }
 
-    public void setUserId(Integer userId) {
-        this.userId = userId;
+    public Integer getUserId() {
+        return userId;
     }
 
     public String getUserName() {
         return userName;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getUserAge() {
+    public Integer getUserAge() {
         return userAge;
-    }
-
-    public void setUserAge(String userAge) {
-        this.userAge = userAge;
     }
 
     public int getPostCode() {
         return postCode;
     }
 
-    public void setPostCode(int postCode) {
-        this.postCode = postCode;
-    }
-
     public String getAdderess() {
         return adderess;
     }
 
-    public void setAdderess(String adderess) {
-        this.adderess = adderess;
-    }
-
     public String getAdderessDetail() {
         return adderessDetail;
-    }
-
-    public void setAdderessDetail(String adderessDetail) {
-        this.adderessDetail = adderessDetail;
     }
 
     @Override
