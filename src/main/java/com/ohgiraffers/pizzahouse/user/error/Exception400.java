2906919a0fbaa928cdba0533872577dd45847b3b
jinny <eujin.mstory@gmail.com>
2024-08-20 11:50:12 +0900

    chore : 코드 수정

--- src/main/java/com/ohgiraffers/pizzahouse/user/error/Exception400.java
+++
@@ -1,9 +0,0 @@
-package com.ohgiraffers.pizzahouse.user.error;
-
-public class Exception400 extends RuntimeException {
-
-    public Exception400(String message) {
-        super(message);
-    }
-
-}
--- src/main/java/com/ohgiraffers/pizzahouse/user/repository/UserRepository.java
+++ src/main/java/com/ohgiraffers/pizzahouse/user/repository/UserRepository.java
@@ -4,9 +4,12 @@ import com.ohgiraffers.pizzahouse.user.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository <UserEntity, Integer> {
 
+    List<UserEntity> findByUserName(String userName);
 
 
 }
